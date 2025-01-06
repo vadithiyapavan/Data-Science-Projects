f79bf84591887390c828a253ede1a0b59211e5f4
VADITHIYA PAVAN KUMAR NAIK <128389135+vadithiyapavan@users.noreply.github.com>
2025-01-06 18:05:30 +0530

    Update app.py

--- bankruptcy/app.py
+++ bankruptcy/app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 # Load the pre-trained model
-pickle_in = open("model.pkl", "rb")
+pickle_in = open("knn.pkl", "rb")
 classifier = pickle.load(pickle_in)
 
 def welcome():
